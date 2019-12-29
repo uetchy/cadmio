09f0bb96c8f4438cb39f1cc1ad1220273e030532
Yasuaki Uechi <y@uechi.io>
2019-12-30 03:39:36 +0900

    chore: add test

---
+++ tests/jsxcad.test.js
@@ -0,0 +1,8 @@
+import JSXCAD from "../src/jsxcad";
+import path from "path";
+import fs from "fs";
+
+it("has JSX properties", () => {
+  expect(JSXCAD.Fragment).toBeDefined();
+  expect(JSXCAD.createElement).toBeDefined();
+});
